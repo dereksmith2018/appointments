5473cf31fa670a129ed91e38627fc69bd9fc3347
Ahmed Khokar and Derek Smith <derek@>
2018-08-08 16:29:05 -0700

    add html to add calender to bootstrap

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,18 @@
 $(document).ready(function(){
-  
+  $("#formOne").submit(function(event){
+    var name = $("input#name").val();
+    var reason = $("input#description").val();
+    var date = $("input#appointment").val();
+    var time = $("input#time").val();
+
+    $("#name5").text(name);
+    $("#describe5").text(reason);
+    $("#date5").text(date);
+    $("#time5").text(time);
+
+    event.preventDefault();
+
+  });
+
+
 });
